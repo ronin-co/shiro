4c2e4c777d0493c949ed3509ba5dcf230ab3621e
Ben <4991309+NuroDev@users.noreply.github.com>
2025-05-26 13:06:04 +0100

    Added initial working Bun adapter

--- packages/shiro-orm/src/adapters/bun.ts
+++ packages/shiro-orm/src/adapters/bun.ts
@@ -1,15 +1,72 @@
 import { Database } from 'bun:sqlite';
+import { ROOT_MODEL, Transaction } from 'shiro-compiler';
 
-import type { Model } from 'shiro-compiler';
+import createSyntaxFactory from '@/src/index';
 
-interface ShiroOptions<T extends Array<Model>> {
+import type { Model, Query } from 'shiro-compiler';
+
+interface ShiroOptions<T extends Array<unknown>> {
+  /**
+   * @todo Add documentation
+   */
   database?: Database;
+
+  /**
+   * @todo Add documentation
+   */
+  experimental?: {
+    /**
+     * @todo Add documentation
+     */
+    initializeModels?: boolean;
+  };
+
+  /**
+   * @todo Add documentation
+   */
   models: T;
 }
 
 /**
  * @todo Add documentation
  */
-export const shiro = <T extends Array<Model>>(options: ShiroOptions<T>) => {
+export const shiro = <const T extends Array<unknown>>(options: ShiroOptions<T>) => {
   const database = options.database ?? new Database(':memory:');
+
+  if (options.experimental?.initializeModels === true) {
+    const models = Object.values(options.models);
+
+    const queries = new Array<Query>({ create: { model: ROOT_MODEL } });
+    for (const model of models) {
+      queries.push({
+        create: {
+          model: model as unknown as Model,
+        },
+      });
+    }
+
+    const transaction = new Transaction(queries);
+    for (const { params, statement } of transaction.statements) {
+      database.query(statement).all(...(params as Array<string>));
+    }
+  }
+
+  return createSyntaxFactory({
+    fetch: async (request) => {
+      if (!(request instanceof Request))
+        throw new Error('Fetcher can only handle Request objects');
+
+      const { nativeQueries } = (await request.json()) as {
+        nativeQueries: Array<{ query: string; values: Array<string> }>;
+      };
+
+      return Response.json({
+        results: nativeQueries.map(({ query, values }) =>
+          database.query(query).all(...values),
+        ),
+      });
+    },
+    token: crypto.randomUUID(),
+    models: options.models as Array<Model>,
+  });
 };
